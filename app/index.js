39f65e5fd07d97bc47cae6d7ab419e2786e24395
David <kadowki@gmail.com>
2014-07-30 19:50:36 -0500

    added a rolldice path to the browser, see readme

--- app/index.js
+++ app/index.js
@@ -23,6 +23,42 @@ app.get('/', function(req, res){
 	res.render('checkers'); //extension is mentioned on line 11
 });
 
+app.get('/add/:x/:y/:z/:n', function(req, res){
+	req.params.x *= 1;
+	req.params.y *= 1;
+	req.params.z *= 1;
+	req.params.n *= 1;
+	req.params.fontsize   = req.query.fontsize;
+	req.params.color	  = req.query.color;
+	req.params.borderwidth= req.query.borderwidth;
+	res.render('sum', req.params);
+
+});
+
+app.get('/sumlist/:list', function(req, res){
+	var sum = 0;
+	req.params.list = req.params.list.split(',');
+	//Kind of like an inline for loop that will run through an array and do something.
+	var list = req.params.list.map(function(x){return x * 1;});
+
+	//Calculate the sum
+	for(var i = 0; i < list.length; i++){
+		sum += list[i]
+	}
+
+	req.params.even = req.query.even;
+	req.params.odd  = req.query.odd;
+	//req.params.list.push(sum);
+	req.params.sum  = sum;
+	res.render('sumlist', req.params);
+});
+
+app.get('/rolldice/:rolls', function(req, res){
+	res.render('rolldice', req.params);
+});
+
+
+
 app.listen(process.env.PORT, function() {
 	console.log('Express.js is listening on port 3000...');
 });
